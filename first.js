48a5abc93ba85225767f13636106c67c0d09de89
Prayas Nag <137247774+123prayas@users.noreply.github.com>
2023-06-21 08:26:50 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,23 @@
+const endDate="22 june 2023 12:00 am"
+document.getElementById('endDate').innerText= endDate;
+const inputs=document.querySelectorAll("input")
+function clock() {
+
+const end=new Date(endDate)
+const now= new Date()
+const diff=(end-now)/1000;
+console.log(diff);
+inputs[0].value=(Math.floor(diff/3600/24));
+inputs[1].value=(Math.floor(diff/3600)%24);
+inputs[2].value=(Math.floor(diff/60)%60);
+inputs[3].value=(Math.floor(diff)%60);
+
+}
+clock()
+
+setInterval (
+    ()=>{
+        clock()
+    },
+    1000
+)
